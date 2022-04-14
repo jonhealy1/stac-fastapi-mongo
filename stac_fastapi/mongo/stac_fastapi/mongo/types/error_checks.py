e81d6d3dd902aad068dd21f380fd98e4f2f0b317
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-13 23:38:38 -0700

    remove session from error checks

--- stac_fastapi/mongo/stac_fastapi/mongo/types/error_checks.py
+++ stac_fastapi/mongo/stac_fastapi/mongo/types/error_checks.py
@@ -3,7 +3,6 @@
 import attr
 from pymongo import MongoClient
 
-from stac_fastapi.mongo.session import Session
 from stac_fastapi.types import stac as stac_types
 from stac_fastapi.types.errors import ConflictError, ForeignKeyError, NotFoundError
 
@@ -11,26 +10,24 @@ from stac_fastapi.types.errors import ConflictError, ForeignKeyError, NotFoundEr
 @attr.s
 class ErrorChecks:
     """error checks class."""
-
-    session: Session = attr.ib(default=Session)
     client: MongoClient = attr.ib(default=None)
 
     def _check_collection_foreign_key(self, model: stac_types.Collection):
         if not self.client.stac.stac_collection.count_documents(
-            {"id": model["collection"]}, limit=1, session=self.session
+            {"id": model["collection"]}, limit=1
         ):
             raise ForeignKeyError(f"Collection {model['collection']} does not exist")
 
     def _check_collection_conflict(self, model: stac_types.Collection):
         if self.client.stac.stac_collection.count_documents(
-            {"id": model["id"]}, limit=1, session=self.session
+            {"id": model["id"]}, limit=1
         ):
             raise ConflictError(f"Collection {model['id']} already exists")
 
     def _check_collection_not_found(self, collection_id: str):
         if (
             self.client.stac.stac_collection.count_documents(
-                {"id": collection_id}, session=self.session
+                {"id": collection_id}
             )
             == 0
         ):
@@ -40,7 +37,6 @@ class ErrorChecks:
         if self.client.stac.stac_item.count_documents(
             {"id": model["id"], "collection": model["collection"]},
             limit=1,
-            session=self.session,
         ):
             raise ConflictError(
                 f"Item {model['id']} in collection {model['collection']} already exists"
@@ -50,7 +46,6 @@ class ErrorChecks:
         if (
             self.client.stac.stac_item.count_documents(
                 {"id": item_id, "collection": collection_id},
-                session=self.session,
             )
             == 0
         ):
